dd8abc2384e60fcaee1e50f3579d3f0eec3e7456
Pablo Tapia <ptapia@proyecto404.com>
2019-08-13 16:03:30 -0300

    kata diaz

--- src/main/kotlin/Alumno.kt
+++
@@ -1,8 +0,0 @@
-package samples
-
-class Alumno(val nombre: String,val apellido: String,val paso: Boolean,val promedio:Double){
-    override fun toString(): String {
-        return "Alumno(nombre='$nombre', apellido='$apellido', paso=$paso, promedio=$promedio)"
-    }
-
-}
--- src/test/kotlin/KataTest.kt
+++ src/test/kotlin/KataTest.kt
@@ -1,9 +1,20 @@
+import org.junit.Assert
 import org.junit.Test
 import kotlin.test.assertTrue
 
 class KataTest {
     @Test
     fun `xxx`() {
-        assertTrue { true }
+        //given()
+        val id = 1
+        //a roberto in position 0 0 and north orientation
+        val robert1= Robert(0,0,"N")
+
+        //when
+        val ttt = SendCommand(listOf(robert1)).execute(id, "F")
+
+
+        val expectedResponse = "10NOK"
+        Assert.assertEquals(expectedResponse, ttt)
     }
 }
